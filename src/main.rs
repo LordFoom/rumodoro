358b76ea8c58085eb0cbece3d1512a5faf9f6a75
lordfoom <timothys@soliditech.com>
2022-04-24 17:46:36 +0200

    Dump gui
    Tui is where it's at, why did I ever question it.
    
    Put it some default libraries I like,
    for pretty stack traces
    and logging, first to stdout but then file,
    and clap for passing in cli params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use color_eyre::eyre::{eyre, Report, Result};
+use std::sync::Once;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+// use tracing_subscriber::filter::
 
 struct Rumodoro{
     // display:String,
@@ -6,8 +11,33 @@ struct Rumodoro{
     // stop_button: button::State,
 }
 
+static INIT: Once = Once::new();
 
+fn setup(verbose:bool)->Result<()>{
+    INIT.call_once(|| {
+        let log_level = if verbose {
+            Level::TRACE
+        } else {
+            Level::ERROR
+        };
+        //print our pretty stack traces by default
+        if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+            std::env::set_var("RUST_LIB_BACKTRACE", "1")
+        }
+        //the library for our pretty stack traces
+        color_eyre::install()?;
+
+
+        let subs = FmtSubscriber::builder()
+            .with_max_level(log_level)
+            .init();
+
+        tracing::subscriber::set_global_default(subs).expect("setting logger failed, m'sieur");
+    });
+    Ok(())
+}
 
 fn main()  {
+
 
 }
