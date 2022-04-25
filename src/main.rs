7e43f614b0599dd1d77f9f040613b8db1c6b7d1f
lordfoom <timothys@soliditech.com>
2022-04-25 18:09:43 +0200

    Draw a block with title
    Soon we will have buttons and time!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::io;
+use std::io::Read;
 use color_eyre::eyre::{eyre, Report, Result};
 use std::sync::Once;
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, execute};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+use tui::backend::{Backend, CrosstermBackend};
+use tui::layout::Alignment;
+use tui::{Frame, Terminal};
+use tui::widgets::{Block, Borders, BorderType};
 // use tracing_subscriber::filter::
+// use crossterm::
 
 struct Rumodoro{
     // display:String,
@@ -14,6 +24,7 @@ struct Rumodoro{
 static INIT: Once = Once::new();
 
 fn setup(verbose:bool)->Result<()>{
+    color_eyre::install()?;
     INIT.call_once(|| {
         let log_level = if verbose {
             Level::TRACE
@@ -25,19 +36,69 @@ fn setup(verbose:bool)->Result<()>{
             std::env::set_var("RUST_LIB_BACKTRACE", "1")
         }
         //the library for our pretty stack traces
-        color_eyre::install()?;
 
 
         let subs = FmtSubscriber::builder()
             .with_max_level(log_level)
-            .init();
+            .finish();
 
         tracing::subscriber::set_global_default(subs).expect("setting logger failed, m'sieur");
     });
+
     Ok(())
 }
 
-fn main()  {
+// fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+
+fn run_the_jewels<B: Backend>(terminal: &mut Terminal<B>) -> Result<()>{
+   loop{
+       terminal.draw(ui);
+
+       if let Event::Key(key) = event::read()? {
+          if let KeyCode::Char('q') = key.code{
+              return Ok(());
+          }
+       }
+   }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>){
+    let size = f.size();
+
+    let clock_block = Block::default()
+        .borders(Borders::ALL)
+        .title("Rumodoro")
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Double);
+    f.render_widget(clock_block, size);
 
 
+}
+
+fn main() -> Result<()>  {
+    //todo we gonna clap this!
+    setup(true)?;
+
+    //enable the app
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture )?;
+    let backend = CrosstermBackend::new(stdout);
+    // // let mut terminal = Terminal::new(backend)?;
+    let mut terminal = Terminal::new(backend)?;
+    // create app and run it
+    let res = run_the_jewels(&mut terminal);
+    //turn off the app, m'sieur
+    disable_raw_mode();
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res{
+        println!("{:?}", err);
+    }
+    Ok(())
 }
